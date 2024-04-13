1a09634561437120a38b99aa5fcbaac2aa8e5809
codingleaf <155798178+codingleaf@users.noreply.github.com>
2024-04-13 11:42:45 +0800

    finish exercise 05_sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,14 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (!Number.isInteger(start) || !Number.isInteger(end)) {
+    return "ERROR";
+  }
+  if (start < 0 || end < 0) {
+    return "ERROR";
+  }
 
+  let n = Math.abs(start - end) + 1;
+  let sum = (n/2) * (start + end);
+  return sum;
 };
 
 // Do not edit below this line
